ae031768807a07c160b9bf20f43cdc56079f5f12
Felipe Rey <juan.valencia.rey@gmail.com>
2024-05-13 13:43:40 -0500

    segundos cambios en pruebas

--- logica.js
+++ logica.js
@@ -1,3 +1,4 @@
 alert("Esto es una ventana emergente ajsjakjdha")
 console.log("Se agrega segunda version desde local")
 console.log("se agrega cambio en rama pruebas")
+console.log("segundos cambios en rama pruebas")
