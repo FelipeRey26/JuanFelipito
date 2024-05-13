46e221f0574d034f6c5c5db69d7837d063584ae7
FelipeRey26 <juan.valencia.rey@correounivalle.edu.co>
2024-05-13 16:27:01 -0500

    se agrega codigo

--- logica.js
+++ logica.js
@@ -1,3 +1,7 @@
+alert("esto es una ventana emergente")
+console.log("se agrega segunda version desde local")
+console.log("se agrega cambio en rama pruebas")
+console.log("segundos cambios en rama pruebas")
 console.log("2181004")
 console.log("taller tecnologico II")
 console.log("Juan Felipe Valencia Rey")
