1902bb5b74f01f53a7fdce70faec37fc87355236
FelipeRey26 <juan.valencia.rey@correounivalle.edu.co>
2024-05-13 16:41:08 -0500

    Update logica.js

--- logica.js
+++ logica.js
@@ -1,7 +1,2 @@
-alert("Esto es una ventana emergente ajsjakjdha")
-console.log("Se agrega segunda version desde local")
-console.log("se agrega cambio en rama pruebas")
-console.log("segundos cambios en rama pruebas")
 console.log("2181004")
-console.log("taller tecnologico II")
 console.log("Juan Felipe Valencia Rey")
