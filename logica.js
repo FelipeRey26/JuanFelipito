ead43f94bfc64604698386eeecad46aef6bb805d
FelipeRey26 <juan.valencia.rey@correounivalle.edu.co>
2024-05-13 16:29:17 -0500

    se agregaron cambios

--- logica.js
+++ logica.js
@@ -1,5 +1,5 @@
-alert("esto es una ventana emergente")
-console.log("se agrega segunda version desde local")
+alert("Esto es una ventana emergente ajsjakjdha")
+console.log("Se agrega segunda version desde local")
 console.log("se agrega cambio en rama pruebas")
 console.log("segundos cambios en rama pruebas")
 console.log("2181004")
